7e2d40ca8f588e7b49524d3fc2c6bb83192301df
Archit Uke <136632956+ukearchit@users.noreply.github.com>
2023-09-21 16:22:59 +0000

    first commit

---
+++ 02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; // Treats it as New Version of JS Code.
+
+// alert statement not correct syntax to use in node.js instead in web browser.
+
+let name = "Archit"
+let age = "17"
+let userLoggedIn = true
+
+/* [Datatypes]
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+*/
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
