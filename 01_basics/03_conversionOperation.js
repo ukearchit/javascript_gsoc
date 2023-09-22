335c9cd5758db89ac60012f5d848bef2fed6ce54
Archit Uke <136632956+ukearchit@users.noreply.github.com>
2023-09-22 07:12:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true
+
+/*console.log(typeof score);
+console.log(typeof (score));*/
+
+let valueInNumber = Number(score)
+/*console.log(typeof valueInNumber);
+console.log(valueInNumber); */
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; , false => 0;
+
+let isLoggedIn = "adam"
+let booleanisLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanisLoggedIn);
+
+// 1 => true; , 0 => false;
+// "" => false;
+// "adam" => true;
+
+let isNum1 = 69;
+let stringisNum1 = String(isNum1);
+console.log(stringisNum1);
+console.log(typeof stringisNum1);
